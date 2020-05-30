0d2b32ff2d469bf3b7fae1c8ace816236ba40eb0
Eugene Shishkin <eshishkin@wiley.com>
2020-05-30 13:08:29 +0400

    issues/3: move url to application.yaml

--- src/main/java/org/eshishkin/edu/cometwatcher/RepositoryConfiguration.java
+++ src/main/java/org/eshishkin/edu/cometwatcher/RepositoryConfiguration.java
@@ -3,6 +3,7 @@ package org.eshishkin.edu.cometwatcher;
 import io.quarkus.arc.DefaultBean;
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eshishkin.edu.cometwatcher.external.JsoupClient;
 import org.eshishkin.edu.cometwatcher.external.JsoupClientImpl;
 import org.eshishkin.edu.cometwatcher.repository.CometExternalRepository;
@@ -11,10 +12,13 @@ import org.eshishkin.edu.cometwatcher.repository.HeavensAboveCometRepository;
 @Dependent
 public class RepositoryConfiguration {
 
+    @ConfigProperty(name = "application.external.url.heavens-above")
+    String heavensAboveBaseUrl;
+
     @Produces
     @DefaultBean
     public CometExternalRepository cometExternalRepository(JsoupClient jsoupClient) {
-        return new HeavensAboveCometRepository("https://www.heavens-above.com", jsoupClient);
+        return new HeavensAboveCometRepository(heavensAboveBaseUrl, jsoupClient);
     }
 
     @Produces
