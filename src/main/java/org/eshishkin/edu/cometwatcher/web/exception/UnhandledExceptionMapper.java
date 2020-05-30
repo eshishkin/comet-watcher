10399280b93bf785b027468857e13e0170d5e1db
Eugene Shishkin <shishkin.john@gmail.com>
2020-05-30 15:55:53 +0400

    Log exception in handler

--- src/main/java/org/eshishkin/edu/cometwatcher/web/exception/UnhandledExceptionMapper.java
+++ src/main/java/org/eshishkin/edu/cometwatcher/web/exception/UnhandledExceptionMapper.java
@@ -3,12 +3,16 @@ package org.eshishkin.edu.cometwatcher.web.exception;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Provider
 public class UnhandledExceptionMapper implements ExceptionMapper<RuntimeException> {
 
     @Override
     public Response toResponse(RuntimeException exception) {
+        log.error("Unhandled exception occurred", exception);
+
         return Response
                 .status(Response.Status.INTERNAL_SERVER_ERROR)
                 .entity(ErrorResponse.of(exception.getMessage()))
