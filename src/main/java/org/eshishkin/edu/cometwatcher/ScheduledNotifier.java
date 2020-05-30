e4b27e10f639347a53efb062a7065e71d0a3951c
Eugene Shishkin <shishkin.john@gmail.com>
2020-05-30 15:57:16 +0400

    https://github.com/eshishkin/comet-watcher/issues/4: generate and send report to email

--- src/main/java/org/eshishkin/edu/cometwatcher/ScheduledNotifier.java
+++ src/main/java/org/eshishkin/edu/cometwatcher/ScheduledNotifier.java
@@ -1,21 +1,51 @@
 package org.eshishkin.edu.cometwatcher;
 
+import io.quarkus.mailer.MailTemplate;
+import io.quarkus.qute.api.ResourcePath;
 import io.quarkus.scheduler.Scheduled;
-import io.quarkus.scheduler.ScheduledExecution;
+import java.time.Instant;
+import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
-import lombok.AllArgsConstructor;
+import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eshishkin.edu.cometwatcher.model.Comet;
 import org.eshishkin.edu.cometwatcher.service.CometService;
 
 @Slf4j
 @ApplicationScoped
-@AllArgsConstructor
 public class ScheduledNotifier {
 
-    private final CometService cometService;
+    @Inject
+    CometService cometService;
 
-    @Scheduled(cron="{application.schedulers.comet-notifier}")
+    @ResourcePath("comets/comet-report")
+    MailTemplate sender;
+
+    @ConfigProperty(name = "application.mail.templates.comet-report.subject")
+    String subject;
+
+    @Scheduled(cron = "{application.schedulers.comet-notifier}")
     public void send() {
-        log.info("Comets found {}", cometService.getComets().size());
+        send("eshishkin@mailinator.com", cometService.getComets());
+    }
+
+    private void send(String recipient, List<Comet> comets) {
+        sender
+                .to(recipient)
+                .subject(subject)
+                .data("comets", comets)
+                .data("generated_at", Instant.now())
+                .send()
+                .toCompletableFuture()
+                .handle((r, ex) -> {
+                    if (ex != null) {
+                        log.error("Unable to send email due to exception", ex);
+                    } else {
+                        log.info("Report has been successfully sent to {}", recipient);
+                    }
+                    return null;
+                })
+                .join();
     }
 }
